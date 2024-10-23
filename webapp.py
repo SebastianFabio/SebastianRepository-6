688cda059dd1be5f211dce44d2218e221efc1631
SebastianFabio <seabass.fabio@gmail.com>
2024-10-23 13:55:57 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,8 +8,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    states = get_state_options()
-    #print(states)
-    return render_template('home.html', state_options=states)
+
+    
+@app.route('/page2')
+def home():
+
+    
+@app.route('/page3')
+def home():
+
+def get_states():
+    """Return a list of state abbreviations from the demographic data."""
+    with open('smoking.json') as smoking_data:
+        counties = json.load(smoking_data)
+    #states=[]
+    #for c in counties:
+        #if c["State"] not in states:
+            #states.append(c["State"])
+    #a more concise but less flexible and less easy to read version is below.
+    states=list(set([c["State"] for c in counties])) #sets do not allow duplicates and the set function is optimized for removing duplicates
+    return states
 
 app.run(debug=False) # change to False when running in production
